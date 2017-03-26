4d802498a3d1253d26a2dce86fd767af671f0b15
Ondrej Sika <ondrej@ondrejsika.com>
2017-03-26 18:57:54 +0200

    redirect / -> /admin/

--- sedastrela_is/urls.py
+++ sedastrela_is/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import include, url
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     url(r'^event/', include('sedastrela_is.event.urls', namespace='event')),
+    url(r'^$', RedirectView.as_view(url='/admin/')),
 ]
-
