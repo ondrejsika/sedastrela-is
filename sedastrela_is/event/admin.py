0afc330cfb4a00dc2768d4b0f253cab2c1120ec4
Ondrej Sika <ondrej@ondrejsika.com>
2017-03-26 15:07:49 +0200

    enhance administration

--- sedastrela_is/event/admin.py
+++ sedastrela_is/event/admin.py
@@ -3,6 +3,44 @@ from django.contrib import admin
 from sedastrela_is.event.models import Event, Attendee, EventNotification
 
 
-admin.site.register(Event)
-admin.site.register(Attendee)
+class AttendeeInline(admin.TabularInline):
+    model = Attendee
+    extra = 0
+
+
+class NotificationInline(admin.TabularInline):
+    model = EventNotification
+    extra = 0
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'from_dt',
+        'to_dt',
+        'price',
+    )
+    inlines = (
+        AttendeeInline,
+        NotificationInline,
+    )
+
+
+class AttendeeAdmin(admin.ModelAdmin):
+    list_display = (
+        'person',
+        'event',
+        'state',
+        'paid',
+    )
+    list_filter = (
+        'person',
+        'event',
+        'state',
+        'paid',
+    )
+
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Attendee, AttendeeAdmin)
 admin.site.register(EventNotification)
--- sedastrela_is/person/admin.py
+++ sedastrela_is/person/admin.py
@@ -3,5 +3,19 @@ from django.contrib import admin
 from sedastrela_is.person.models import Person
 
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'nick_name',
+        'phone',
+        'email',
+        'mother_phone',
+        'mother_email',
+        'father_phone',
+        'father_email',
+    )
+
+
+admin.site.register(Person, PersonAdmin)
 
